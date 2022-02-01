af6ad1f330c28149adb141cf88fddc6d47d61e7d
Gabe Woods <52386910+Maerin8@users.noreply.github.com>
2022-02-01 14:03:33 -0600

    Create ProEul5.py

---
+++ PE1-9/ProEul5.py
@@ -0,0 +1,15 @@
+#https://projecteuler.net/problem=5
+
+def isDivisible(num):
+    for i in range(1, 21):
+        if num % i != 0:
+            return False
+    return True
+
+num = 20 #starting number
+while True:
+    if isDivisible(num):
+        break
+    print("{:,}".format(num))
+    num+=20 #add by 20 since it has to be divisible by 20
+		#makes it run 20x faster
